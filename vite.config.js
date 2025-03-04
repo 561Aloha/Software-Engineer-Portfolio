2dfce00b184bad0a6ea95b241e7fb2839c787359
Dianna D <112281080+561Aloha@users.noreply.github.com>
2025-03-04 17:56:38 -0500

    Fixed react-router-dom import issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "react-router-dom": "/node_modules/react-router-dom/"
+    }
+  }
 });
